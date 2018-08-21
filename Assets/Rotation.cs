2f1d5faf263298c01e36a56620b8966f30163bb0
Tyler Molander <tylermolander@gmail.com>
2018-08-21 10:53:12 -0400

    Snapping

---
+++ Assets/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    public float speed = 0.3f;
+	
+	void Update ()
+    {
+       //transform.rotation = new Vector3(speed, speed, speed);
+	}
+}
