2d87e33cc009a640c2bbbe0ac5fa6111f79bb38a
Tyler Molander <tylermolander@gmail.com>
2018-08-21 12:38:38 -0400

    CubeLabels

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Start()
+    {
+
+    }
 
     void Update()
     {
         Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; //snaps by a factor of 10
 
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; //snaps by a factor of 10
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
